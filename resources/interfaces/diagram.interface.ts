12cff2ff0cdf33e4e5a7182d7d48a5198d8e5ac8
Hyubs Ursua <hyubs.u@gmail.com>
2023-07-28 14:19:34 +0800

    fix: missing properties in interfaces

--- resources/interfaces/diagram.interface.ts
+++ resources/interfaces/diagram.interface.ts
@@ -2,5 +2,6 @@ import type { Node } from "./node.interface";
 
 export interface Diagram {
   variables: Record<string, string>;
+  startNodeID: string;
   nodes: Record<string, Node>;
 }
--- resources/interfaces/node.interface.ts
+++ resources/interfaces/node.interface.ts
@@ -3,6 +3,7 @@ export type Node = TextNode;
 export interface TextNode {
   type: "text";
   value: NodeMarkup;
+  nextID?: string;
 }
 
 export type NodeMarkup = string | Array<string | { variableID: string }>;
